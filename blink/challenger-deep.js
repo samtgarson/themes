2bb7a03f2ad7d25c786cbfd32e9478566665ef3b
Sam Garson <samtgarson@gmail.com>
2020-01-09 23:00:19 +0000

    Night Owl and Challenger Deep blink themes

---
+++ blink/challenger-deep.js
@@ -0,0 +1,23 @@
+t.prefs_.set('color-palette-overrides', [
+  '#100e23', // black (0)
+  '#ff5458', // red (1)
+  '#62d196', // green (2)
+  '#ffb378', // yellow (3)
+  '#65b2ff', // blue (4)
+  '#906cff', // pink (5)
+  '#63f2f1', // cyan (6)
+  '#a6b3cc', // white (7)
+  '#565575', // light black (8)
+  '#ff8080', // light red (9)
+  '#95ffa4', // light green (10)
+  '#ffe9aa', // light yellow (11)
+  '#91ddff', // light blue (12)
+  '#c991e1', // light pink (13)
+  '#aaffe4', // light cyan (14)
+  '#cbe3e7'  // light white (15)
+]);
+
+t.prefs_.set('cursor-color', '#fbfcfc');
+t.prefs_.set('foreground-color', '#cbe3e7');
+t.prefs_.set('background-color', '#1e1c31');
+
---
+++ blink/night-owl.js
@@ -0,0 +1,23 @@
+t.prefs_.set('color-palette-overrides', [
+  '#011627', // black (0)
+  '#EF5350', // red (1)
+  '#22da6e', // green (2)
+  '#addb67', // yellow (3)
+  '#82aaff', // blue (4)
+  '#c792ea', // magenta (5)
+  '#21c7a8', // cyan (6)
+  '#ffffff', // white (7)
+  '#575656', // light black (8)
+  '#ef5350', // light red (9)
+  '#22da6e', // light green (10)
+  '#ffeb95', // light yellow (11)
+  '#82aaff', // light blue (12)
+  '#c792ea', // light magenta (13)
+  '#7fdbca', // light cyan (14)
+  '#ffffff', // light white (15)
+]);
+
+t.prefs_.set('cursor-color', '#7e57c2');
+t.prefs_.set('foreground-color', '#d6deeb');
+t.prefs_.set('background-color', '#011627');
+
